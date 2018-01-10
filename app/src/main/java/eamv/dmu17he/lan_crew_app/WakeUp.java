7c7957d9f264eb3c190a961c06dfde8dd9d87a46
kebabjohn <32761420+kebabjohn@users.noreply.github.com>
2018-01-10 14:13:08 +0100

    WakeUp UI

---
+++ app/src/main/java/eamv/dmu17he/lan_crew_app/WakeUp.java
@@ -0,0 +1,13 @@
+package eamv.dmu17he.lan_crew_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WakeUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wake_up);
+    }
+}
