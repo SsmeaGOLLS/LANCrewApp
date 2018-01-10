6f56136b85d97a5164038269146f9f3b780b59fc
Jonaskjeld <Jonaskj@live.dk>
2018-01-10 09:28:00 +0100

    Upload to git
    Uploaded the design of the app

---
+++ app/src/main/java/eamv/dmu17he/lan_crew_app/ContactsActivity.java
@@ -0,0 +1,13 @@
+package eamv.dmu17he.lan_crew_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContactsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
+    }
+}
--- app/src/main/java/eamv/dmu17he/lan_crew_app/MainActivity.java
+++ app/src/main/java/eamv/dmu17he/lan_crew_app/MainActivity.java
@@ -1,13 +1,79 @@
 package eamv.dmu17he.lan_crew_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner spinner;
+    ArrayAdapter<CharSequence> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+        adapter = ArrayAdapter.createFromResource(this, R.array.options,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+               /*   switch(i){
+                      case 0:
+                           goToProfileActivity();
+                        case 1:
+                             goToContactsActivity();
+                      } */
+
+                Toast.makeText(getBaseContext(), adapterView.getItemIdAtPosition(i) + " selected", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+        Button schedule = (Button) findViewById(R.id.schedule);
+        schedule.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                goToScheduleActivity();
+            }
+        });
+
+        Button contacts = (Button) findViewById(R.id.contacts);
+        contacts.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                goToContactsActivity();
+            }
+        });
+    }
+    private void goToScheduleActivity(){
+        Intent intent = new Intent(this, ScheduleActivity.class);
+        startActivity(intent);
+    }
+
+    private void goToContactsActivity(){
+        Intent intent = new Intent(this, ContactsActivity.class);
+        startActivity(intent);
+    }
+
+
+    private void goToProfileActivity(){
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
     }
 }
