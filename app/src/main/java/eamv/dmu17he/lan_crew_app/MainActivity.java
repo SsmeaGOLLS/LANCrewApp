e0a7ee363a290bfb5f33fda63f78000c2d97546d
Jonaskjeld <Jonaskj@live.dk>
2018-01-10 10:27:53 +0100

    Fixed design of mainmenu

--- app/src/main/java/eamv/dmu17he/lan_crew_app/MainActivity.java
+++ app/src/main/java/eamv/dmu17he/lan_crew_app/MainActivity.java
@@ -1,6 +1,7 @@
 package eamv.dmu17he.lan_crew_app;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
