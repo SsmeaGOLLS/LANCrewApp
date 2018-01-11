87011a10099a7439738d81f3ad2c328ced292930
kebabjohn <32761420+kebabjohn@users.noreply.github.com>
2018-01-11 15:30:44 +0100

    more activities

--- app/src/main/java/eamv/dmu17he/lancrewapp/activities/PickSpace.java
+++ app/src/main/java/eamv/dmu17he/lancrewapp/activities/PickSpace.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 import eamv.dmu17he.lancrewapp.R;
 
-public class Bookings extends AppCompatActivity {
+public class PickSpace extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bookings);
+        setContentView(R.layout.activity_pickspace);
     }
 }
