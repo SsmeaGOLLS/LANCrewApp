b0de8abe9828e5add6783faf8b65e971e19311f5
kebabjohn <32761420+kebabjohn@users.noreply.github.com>
2018-01-15 09:39:50 +0100

    Made menu button go to or wake up start screen

--- app/src/main/java/eamv/dmu17he/lancrewapp/activities/MenuActivity.java
+++ app/src/main/java/eamv/dmu17he/lancrewapp/activities/MenuActivity.java
@@ -1,8 +1,11 @@
 package eamv.dmu17he.lancrewapp.activities;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import eamv.dmu17he.lancrewapp.R;
 
@@ -12,6 +15,15 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        Button vækningBtn = findViewById(R.id.vækningBtn);
+        vækningBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuActivity.this, ActivityWakeUp.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
