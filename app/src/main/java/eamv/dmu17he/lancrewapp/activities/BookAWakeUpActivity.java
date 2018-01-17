63ceb1e8e18ae416c73c9758b481a7bd95a00b98
kebabjohn <32761420+kebabjohn@users.noreply.github.com>
2018-01-17 12:16:25 +0100

    Maybe it works

--- app/src/main/java/eamv/dmu17he/lancrewapp/activities/BookAWakeUpActivity.java
+++ app/src/main/java/eamv/dmu17he/lancrewapp/activities/BookAWakeUpActivity.java
@@ -3,6 +3,7 @@ package eamv.dmu17he.lancrewapp.activities;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,20 +16,24 @@ import android.widget.TextView;
 
 import com.microsoft.windowsazure.mobileservices.MobileServiceClient;
 import com.microsoft.windowsazure.mobileservices.table.MobileServiceTable;
+import com.microsoft.windowsazure.mobileservices.table.sync.MobileServiceSyncContext;
+import com.microsoft.windowsazure.mobileservices.table.sync.localstore.ColumnDataType;
+import com.microsoft.windowsazure.mobileservices.table.sync.localstore.MobileServiceLocalStoreException;
+import com.microsoft.windowsazure.mobileservices.table.sync.localstore.SQLiteLocalStore;
+import com.microsoft.windowsazure.mobileservices.table.sync.synchandler.SimpleSyncHandler;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 import eamv.dmu17he.lancrewapp.R;
 import eamv.dmu17he.lancrewapp.helper.AzureServiceAdapter;
-import eamv.dmu17he.lancrewapp.helper.BookingListAdapter;
 import eamv.dmu17he.lancrewapp.helper.ToDialogError;
 import eamv.dmu17he.lancrewapp.model.Controller;
-import eamv.dmu17he.lancrewapp.model.Hall;
-import eamv.dmu17he.lancrewapp.model.Space;
 import eamv.dmu17he.lancrewapp.model.WakeUp;
 
 
@@ -36,10 +41,7 @@ public class BookAWakeUpActivity extends AppCompatActivity implements AdapterVie
 
     private MobileServiceClient mClient;
     private MobileServiceTable<WakeUp> mWakeUpTable;
-    private MobileServiceTable<Hall> mHallTable;
-    private MobileServiceTable<Space> mSpaceTable;
     private ProgressBar mProgressBar;
-    private BookingListAdapter mBookingAdapter;
     private AzureServiceAdapter mAzureAdapter;
 
     private ArrayList<String> wakeupss;
@@ -49,9 +51,14 @@ public class BookAWakeUpActivity extends AppCompatActivity implements AdapterVie
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_awake_up);
+
         initProgressBar();
         initMobileService();
-
+        try {
+            initLocalStore();
+        } catch (InterruptedException | ExecutionException | MobileServiceLocalStoreException e) {
+            e.printStackTrace();
+        }
 
         createTimesForWakeups();
         bookWakeUp();
@@ -85,9 +92,7 @@ public class BookAWakeUpActivity extends AppCompatActivity implements AdapterVie
         acceptWakeUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Controller.getInstance().createWakeUp("find out how to get username", selectedWakeUpTime, getComment());
                 AddWakeUp();
-
             }
         });
     }
@@ -103,8 +108,6 @@ public class BookAWakeUpActivity extends AppCompatActivity implements AdapterVie
                     test.setComment(getComment());
                     test.setTime(selectedWakeUpTime.getTime().toString());
                     WakeUp entity = mWakeUpTable.insert(test).get();
-
-
                 } catch (InterruptedException | ExecutionException e) {
                     ToDialogError.getInstance().createAndShowDialogFromTask(e, "Error", mActivity);
                     e.printStackTrace();
@@ -114,7 +117,6 @@ public class BookAWakeUpActivity extends AppCompatActivity implements AdapterVie
         };
         task.execute();
     }
-
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
@@ -126,13 +128,57 @@ public class BookAWakeUpActivity extends AppCompatActivity implements AdapterVie
         } catch (ParseException e) {
             e.printStackTrace();
         }
-
         selectedWakeUpTime = calendar;
-
     }
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
         // Another interface callback
+    }
+
+    private AsyncTask<Void, Void, Void> initLocalStore() throws MobileServiceLocalStoreException, ExecutionException, InterruptedException {
+        final Activity mActivity = this;
+        @SuppressLint("StaticFieldLeak") // <-- Just to suppress warning
+                AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+
+                    MobileServiceSyncContext syncContext = mClient.getSyncContext();
+
+                    if (syncContext.isInitialized())
+                        return null;
+
+                    SQLiteLocalStore localStoreWakeUp = new SQLiteLocalStore(mClient.getContext(), "OfflineStore", null, 1);
+
+                    Map<String, ColumnDataType> tableDefinitionWakeUp = new HashMap<String, ColumnDataType>();
+                    tableDefinitionWakeUp.put("id", ColumnDataType.String);
+                    tableDefinitionWakeUp.put("startTime", ColumnDataType.DateTimeOffset);
+                    tableDefinitionWakeUp.put("endTime", ColumnDataType.DateTimeOffset);
+                    tableDefinitionWakeUp.put("pokeCounter", ColumnDataType.Integer);
+                    tableDefinitionWakeUp.put("time", ColumnDataType.String);
+                    tableDefinitionWakeUp.put("comment", ColumnDataType.String);
+
+                    localStoreWakeUp.defineTable("WakeUp", tableDefinitionWakeUp);
+
+                    SimpleSyncHandler handlerWakeUp = new SimpleSyncHandler();
+
+                    syncContext.initialize(localStoreWakeUp, handlerWakeUp).get();
+
+                } catch (final Exception e) {
+                    ToDialogError.getInstance().createAndShowDialogFromTask(e, "Error", mActivity);
+                }
+                return null;
+            }
+        };
+        return runAsyncTask(task);
+    }
+
+    public AsyncTask<Void, Void, Void> runAsyncTask(AsyncTask<Void, Void, Void> task) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            return task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+        } else {
+            return task.execute();
+        }
     }
 
     private void initProgressBar() {
@@ -144,9 +190,6 @@ public class BookAWakeUpActivity extends AppCompatActivity implements AdapterVie
         mAzureAdapter = AzureServiceAdapter.getInstance();
         mAzureAdapter.updateClient(this, this, mProgressBar);
         mClient = mAzureAdapter.getClient();
-        mBookingAdapter = new BookingListAdapter(this, R.layout.bookinglistitem);
-        mSpaceTable = mClient.getTable(Space.class);
-        mHallTable = mClient.getTable(Hall.class);
         mWakeUpTable = mClient.getTable(WakeUp.class);
     }
 }
