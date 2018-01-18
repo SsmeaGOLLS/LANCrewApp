9d66b0555e3b864635345ce50f05bc6464a1539a
JTKlemmensen <25729544+JTKlemmensen@users.noreply.github.com>
2018-01-18 12:34:50 +0100

    removed useless method

--- app/src/main/java/eamv/dmu17he/lancrewapp/activities/CrewMessageActivity.java
+++ app/src/main/java/eamv/dmu17he/lancrewapp/activities/CrewMessageActivity.java
@@ -159,31 +159,4 @@ public class CrewMessageActivity extends Activity {
             return task.execute();
         }
     }
-
-    private class ProgressFilter implements ServiceFilter {
-
-        @Override
-        public ListenableFuture<ServiceFilterResponse> handleRequest(ServiceFilterRequest request, NextServiceFilterCallback nextServiceFilterCallback) {
-
-            final SettableFuture<ServiceFilterResponse> resultFuture = SettableFuture.create();
-
-
-            ListenableFuture<ServiceFilterResponse> future = nextServiceFilterCallback.onNext(request);
-
-            Futures.addCallback(future, new FutureCallback<ServiceFilterResponse>() {
-                @Override
-                public void onFailure(Throwable e) {
-                    resultFuture.setException(e);
-                }
-
-                @Override
-                public void onSuccess(ServiceFilterResponse response) {
-
-                    resultFuture.set(response);
-                }
-            });
-
-            return resultFuture;
-        }
-    }
 }
